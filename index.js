1c03c4edb9a70044570348ac4f78c63b33a41d8c
jahir <nockjahirul@gmail.com>
2025-04-17 06:46:50 +0600

    add app get application specpice id

--- index.js
+++ index.js
@@ -50,6 +50,12 @@ async function run() {
     const cursor = applicationCollection.find();
     const result = await cursor.toArray();
     res.send(result);
+  });
+  app.get('/aplications/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query ={_id:new Object(id)};
+    const result = await applicationCollection.findOne(query);
+    res.send(result);
   })
 
 
